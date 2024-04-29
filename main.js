119a16c9c2d5bfbb9ad897664fe65609f7381190
DmutroZel <156704336+DmutroZel@users.noreply.github.com>
2024-04-29 16:57:11 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+box.onclick = function () {
+    box.style.display = 'none';
+}
+box2.onclick = function () {
+    box2.style.display = 'none';
+}
+box3.onclick = function () {
+    box3.style.display = 'none';
+}
+box4.onclick = function () {
+    box4.style.display = 'none';
+}
